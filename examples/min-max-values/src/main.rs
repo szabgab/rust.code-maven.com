5ffa9de738d5e79f65024d4e3df6983464f9198e
Gabor Szabo <gabor@szabgab.com>
2023-10-03 07:05:33 +0300

    numeric MIN and MAX

---
+++ examples/min-max-values/src/main.rs
@@ -0,0 +1,31 @@
+fn main() {
+    println!("u8::MIN {}", u8::MIN);
+    println!("u8::MAX {}", u8::MAX);
+
+    println!("i8::MIN {}", i8::MIN);
+    println!("i8::MAX {}", i8::MAX);
+
+    println!("u64::MIN {}", u64::MIN);
+    println!("u64::MAX {}", u64::MAX);
+
+    println!("i64::MIN {}", i64::MIN);
+    println!("i64::MAX {}", i64::MAX);
+
+    println!("u128::MIN {}", u128::MIN);
+    println!("u128::MAX {}", u128::MAX);
+
+    println!("i128::MIN {}", i128::MIN);
+    println!("i128::MAX {}", i128::MAX);
+
+    println!("f32::MIN {}", f32::MIN);
+    println!("f32::MAX {}", f32::MAX);
+
+    println!("f64::MIN {}", f64::MIN);
+    println!("f64::MAX {}", f64::MAX);
+
+    println!("usize::MIN {}", usize::MIN);
+    println!("usize::MAX {}", usize::MAX);
+
+    println!("isize::MIN {}", isize::MIN);
+    println!("isize::MAX {}", isize::MAX);
+}
