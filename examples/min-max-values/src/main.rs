708447454e60c579d394a899ddf47352c64820bb
Gabor Szabo <gabor@szabgab.com>
2023-12-19 08:21:23 +0200

    update list of types and results

--- examples/min-max-values/src/main.rs
+++ examples/min-max-values/src/main.rs
@@ -1,31 +1,44 @@
 fn main() {
     println!("u8::MIN {}", u8::MIN);
     println!("u8::MAX {}", u8::MAX);
+    println!();
 
     println!("i8::MIN {}", i8::MIN);
     println!("i8::MAX {}", i8::MAX);
+    println!();
+
+    println!("u16::MIN {}", u16::MIN);
+    println!("u16::MAX {}", u16::MAX);
+    println!();
+
+    println!("i16::MIN {}", i16::MIN);
+    println!("i16::MAX {}", i16::MAX);
+    println!();
 
     println!("u64::MIN {}", u64::MIN);
     println!("u64::MAX {}", u64::MAX);
+    println!();
 
     println!("i64::MIN {}", i64::MIN);
     println!("i64::MAX {}", i64::MAX);
+    println!();
 
     println!("u128::MIN {}", u128::MIN);
     println!("u128::MAX {}", u128::MAX);
-
+    println!();
     println!("i128::MIN {}", i128::MIN);
     println!("i128::MAX {}", i128::MAX);
-
+    println!();
     println!("f32::MIN {}", f32::MIN);
     println!("f32::MAX {}", f32::MAX);
-
+    println!();
     println!("f64::MIN {}", f64::MIN);
     println!("f64::MAX {}", f64::MAX);
-
+    println!();
     println!("usize::MIN {}", usize::MIN);
     println!("usize::MAX {}", usize::MAX);
-
+    println!();
     println!("isize::MIN {}", isize::MIN);
     println!("isize::MAX {}", isize::MAX);
+    println!();
 }
