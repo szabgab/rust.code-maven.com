330c2fa6f72d62747c7c7438c362029817de25ac
Gabor Szabo <gabor@szabgab.com>
2024-01-07 20:00:08 +0200

    overflow

--- examples/overflow/src/main.rs
+++ examples/overflow/src/main.rs
@@ -1,20 +1,20 @@
 fn main() {
-    // let mut count: u8 = 254;
-    // println!("count: {}", count);
-    // for _ in 1..=3 {
-    //     count += 1;
-    //     println!("count: {}", count);
-    // }
+    let mut count: u8 = 254;
+    println!("count: {}", count);
+    for _ in 1..=3 {
+        count += 1;
+        println!("count: {}", count);
+    }
 
-    // let mut count: u8 = 254;
-    // println!("count: {}", count);
-    // for _ in 1..=3 {
-    //     match count.checked_add(1) {
-    //         Some(val) => count = val,
-    //         None => eprintln!("Too much!"),
-    //     };
-    //     println!("count: {}", count);
-    // }
+    let mut count: u8 = 254;
+    println!("count: {}", count);
+    for _ in 1..=3 {
+        match count.checked_add(1) {
+            Some(val) => count = val,
+            None => eprintln!("Too much!"),
+        };
+        println!("count: {}", count);
+    }
 
 
     let mut count: u8 = 254;
