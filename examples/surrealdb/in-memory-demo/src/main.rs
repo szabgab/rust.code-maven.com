004e03d89af44d1e9ba2694a7c2f151a05bdcd8c
Gabor Szabo <gabor@szabgab.com>
2024-01-10 11:52:24 +0200

    add data

--- examples/surrealdb/in-memory-demo/src/main.rs
+++ examples/surrealdb/in-memory-demo/src/main.rs
@@ -1,9 +1,33 @@
+use surrealdb::engine::local::{Db, Mem};
 use surrealdb::Surreal;
-use surrealdb::engine::local::Mem;
 
 #[tokio::main]
 async fn main() -> surrealdb::Result<()> {
-    let _db = Surreal::new::<Mem>(()).await?;
+    let db = Surreal::new::<Mem>(()).await?;
+
+    db.use_ns("test_ns").use_db("test_db").await?;
+
+    add_to(db).await?;
 
     Ok(())
+}
+
+async fn add_to(db: Surreal<Db>) -> surrealdb::Result<()> {
+    let data = ("Foo", "12345");
+
+    let (name, phone) = data;
+
+    let response = db
+        .query("CREATE entry SET  name=$name, phone=$phone")
+        .bind(("name", name))
+        .bind(("phone", phone))
+        .await?;
+
+    match response.check() {
+        Ok(_) => return Ok(()),
+        Err(err) => {
+            eprintln!("Could not add entry {}", err);
+            std::process::exit(2);
+        }
+    };
 }
