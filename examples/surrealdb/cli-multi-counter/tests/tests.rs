d689689c930ac830b8012abf7f7ce02389504be3
Gabor Szabo <gabor@szabgab.com>
2024-01-16 15:03:34 +0200

    add tests

--- examples/surrealdb/cli-multi-counter/tests/tests.rs
+++ examples/surrealdb/cli-multi-counter/tests/tests.rs
@@ -11,85 +11,37 @@ fn test_counter() {
 
     std::env::set_var("DATABASE_PATH", tmp_dir.path());
 
+    check("foo", "1\n");
+    check("foo", "2\n");
+    check("foo", "3\n");
+    check("bar", "1\n");
+    check("bar", "2\n");
+    check("foo", "4\n");
+
     let result = Command::new("cargo")
-        .args(["run", "-q", "foo"])
+        .args(["run", "-q"])
         .output()
         .expect("command failed to start");
 
-    assert_eq!(std::str::from_utf8(&result.stdout).unwrap(), "1\n");
+    assert_eq!(
+        std::str::from_utf8(&result.stdout).unwrap(),
+        "Listing counters\n----------------\nfoo: 4\nbar: 2\n"
+    );
     assert_eq!(std::str::from_utf8(&result.stderr).unwrap(), "");
     assert_eq!(result.status, ExitStatus::from_raw(0));
-
-    let result = Command::new("cargo")
-        .args(["run", "-q", "foo"])
-        .output()
-        .expect("command failed to start");
-
-    assert_eq!(std::str::from_utf8(&result.stdout).unwrap(), "2\n");
-    assert_eq!(std::str::from_utf8(&result.stderr).unwrap(), "");
-    assert_eq!(result.status, ExitStatus::from_raw(0));
-
-
-    // let result = Command::new("cargo")
-    //     .args(["run", "-q", "add", "foo", "789"])
-    //     .output()
-    //     .expect("command failed to start");
-
-    // assert_eq!(std::str::from_utf8(&result.stdout).unwrap(), "");
-    // assert!(std::str::from_utf8(&result.stderr).unwrap().contains(
-    //     "Could not add entry Database index `entry_email` already contains 'foo', with record"
-    // ));
-    // assert_eq!(result.status, ExitStatus::from_raw(2 * 256));
-
-    // let result = Command::new("cargo")
-    //     .args(["run", "-q", "show", "foo"])
-    //     .output()
-    //     .expect("command failed to start");
-
-    // assert_eq!(std::str::from_utf8(&result.stdout).unwrap(), "foo: 123\n");
-    // assert_eq!(std::str::from_utf8(&result.stderr).unwrap(), "");
-    // assert_eq!(result.status, ExitStatus::from_raw(0));
-
-    // let result = Command::new("cargo")
-    //     .args(["run", "-q", "show", "bar"])
-    //     .output()
-    //     .expect("command failed to start");
-
-    // assert_eq!(std::str::from_utf8(&result.stdout).unwrap(), "bar: 456\n");
-    // assert_eq!(std::str::from_utf8(&result.stderr).unwrap(), "");
-    // assert_eq!(result.status, ExitStatus::from_raw(0));
-
-    // let result = Command::new("cargo")
-    //     .args(["run", "-q", "list"])
-    //     .output()
-    //     .expect("command failed to start");
-
-    // //println!("{}", std::str::from_utf8(&result.stdout).unwrap());
-    // assert_eq!(
-    //     std::str::from_utf8(&result.stdout).unwrap(),
-    //     "bar: 456\nfoo: 123\n"
-    // );
-    // assert_eq!(std::str::from_utf8(&result.stderr).unwrap(), "");
-    // assert_eq!(result.status, ExitStatus::from_raw(0));
-
-    // let result = Command::new("cargo")
-    //     .args(["run", "-q", "delete", "foo"])
-    //     .output()
-    //     .expect("command failed to start");
-
-    // assert_eq!(std::str::from_utf8(&result.stdout).unwrap(), "");
-    // assert_eq!(std::str::from_utf8(&result.stderr).unwrap(), "");
-    // assert_eq!(result.status, ExitStatus::from_raw(0));
-
-    // let result = Command::new("cargo")
-    //     .args(["run", "-q", "list"])
-    //     .output()
-    //     .expect("command failed to start");
-
-    // //println!("{}", std::str::from_utf8(&result.stdout).unwrap());
-    // assert_eq!(std::str::from_utf8(&result.stdout).unwrap(), "bar: 456\n");
-    // assert_eq!(std::str::from_utf8(&result.stderr).unwrap(), "");
-    // assert_eq!(result.status, ExitStatus::from_raw(0));
 
     drop(tmp_dir);
 }
+
+fn check(name: &str, expected_stdout: &str) {
+
+    let result = Command::new("cargo")
+        .args(["run", "-q", name])
+        .output()
+        .expect("command failed to start");
+
+    assert_eq!(std::str::from_utf8(&result.stdout).unwrap(), expected_stdout);
+    assert_eq!(std::str::from_utf8(&result.stderr).unwrap(), "");
+    assert_eq!(result.status, ExitStatus::from_raw(0));
+}
+
