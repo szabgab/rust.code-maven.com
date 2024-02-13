97b01b44e594dc67c742cd1541edf0bf8e25dc07
Gabor Szabo <gabor@szabgab.com>
2024-02-13 16:54:27 +0200

    publish

---
+++ examples/regex-replace-text-by-text/src/main.rs
@@ -0,0 +1,51 @@
+use regex::{Captures, Regex};
+
+fn main() {
+    let text = String::from("abc123def");
+
+    // replace every sequnce of letters by x (but not the digits)
+    let re = Regex::new(r"[A-Za-z]+").unwrap();
+    let result = re.replace_all(&text, "x");
+    println!("{result}"); // x123x
+
+    // replace every 3 characters by a y
+    let re = Regex::new(r"...").unwrap();
+    let result = re.replace_all(&text, "y");
+    println!("{result}"); // yyy
+
+    // Swap every two charcters
+    let re = Regex::new(r"(.)(.)").unwrap();
+    let result = re.replace_all(&text, r#"$2$1"#);
+    println!("{result}"); // ba1c32edf
+
+    // duplicate every digit
+    let re = Regex::new(r"(\d)").unwrap();
+    let result = re.replace_all(&text, r#"$1$1"#);
+    println!("{result}"); // abc112233def
+
+    // double every digit
+    // Replace using a function to generated the replacement
+    let re = Regex::new(r"(\d)").unwrap();
+    let result = re.replace_all(&text, double);
+    println!("{result}"); // abc246def
+
+    // double every digit
+    // The same inlined
+    let re = Regex::new(r"(\d)").unwrap();
+    let result = re.replace_all(&text, |caps: &Captures| {
+        format!(
+            "{}",
+            2 * caps.get(1).unwrap().as_str().parse::<u8>().unwrap()
+        )
+    });
+    println!("{result}"); // abc246def
+}
+
+fn double(caps: &Captures) -> String {
+    //String::new()
+    //format!("{}", 2 * caps.get(1).unwrap().as_str().parse::<u8>().unwrap())
+    format!(
+        "{}",
+        2 * caps.get(1).unwrap().as_str().parse::<u8>().unwrap()
+    )
+}
---
+++ pages/regex-replace-string.md
@@ -0,0 +1,24 @@
+---
+title: Regex - replace part of the string
+timestamp: 2024-02-13T17:20:01
+author: szabgab
+published: true
+description: Regular expression are powerful.
+tags:
+    - Regex
+    - Captures
+    - replace_all
+todo:
+    - TODO
+---
+
+
+## Dependencies
+
+![](examples/regex-replace-text-by-text/Cargo.toml)
+
+
+## Full code
+
+![](examples/regex-replace-text-by-text/src/main.rs)
+
