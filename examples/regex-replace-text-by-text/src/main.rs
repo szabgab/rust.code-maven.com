c253736f6592894963643a14f54ae217a3897ded
Gabor Szabo <gabor@szabgab.com>
2024-02-13 17:12:27 +0200

    update

--- examples/regex-replace-text-by-text/src/main.rs
+++ examples/regex-replace-text-by-text/src/main.rs
@@ -1,36 +1,41 @@
 use regex::{Captures, Regex};
 
 fn main() {
-    let text = String::from("abc123def");
 
     // replace every sequnce of letters by x (but not the digits)
+    let text = String::from("abc123def");
     let re = Regex::new(r"[A-Za-z]+").unwrap();
     let result = re.replace_all(&text, "x");
     println!("{result}"); // x123x
 
     // replace every 3 characters by a y
+    let text = String::from("abc123def");
     let re = Regex::new(r"...").unwrap();
     let result = re.replace_all(&text, "y");
     println!("{result}"); // yyy
 
     // Swap every two charcters
+    let text = String::from("abc123def");
     let re = Regex::new(r"(.)(.)").unwrap();
     let result = re.replace_all(&text, r#"$2$1"#);
     println!("{result}"); // ba1c32edf
 
     // duplicate every digit
+    let text = String::from("abc123def");
     let re = Regex::new(r"(\d)").unwrap();
     let result = re.replace_all(&text, r#"$1$1"#);
     println!("{result}"); // abc112233def
 
     // double every digit
     // Replace using a function to generated the replacement
+    let text = String::from("abc123def");
     let re = Regex::new(r"(\d)").unwrap();
     let result = re.replace_all(&text, double);
     println!("{result}"); // abc246def
 
     // double every digit
     // The same inlined
+    let text = String::from("abc123def");
     let re = Regex::new(r"(\d)").unwrap();
     let result = re.replace_all(&text, |caps: &Captures| {
         format!(
