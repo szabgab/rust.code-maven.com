8fd6033c15b569100e75162016417dacfcc27371
Gabor Szabo <gabor@szabgab.com>
2024-02-20 13:38:56 +0200

    update

--- examples/parse-curly/src/main.rs
+++ examples/parse-curly/src/main.rs
@@ -1,5 +1,5 @@
+use regex::RegexBuilder;
 use std::collections::HashMap;
-use regex::Regex;
 
 #[derive(Debug, PartialEq, Eq)]
 #[allow(dead_code)]
@@ -17,28 +17,118 @@ impl Default for Curly {
     }
 }
 
-
 fn main() {
     let res = parse_curly(r#"{%  qqrq    %}"#);
-    assert_eq!(res, Some(Curly { name: String::from("qqrq"), ..Curly::default()}));
+    println!("{:?}\n", res);
+    assert_eq!(
+        res,
+        Some(Curly {
+            name: String::from("qqrq"),
+            ..Curly::default()
+        })
+    );
 
     let res = parse_curly(r#"{%  include   file="example/code.rs" %}"#);
+    println!("{:?}\n", res);
+    assert_eq!(
+        res,
+        Some(Curly {
+            name: String::from("include"),
+            fields: HashMap::from([(String::from("file"), String::from("example/code.rs"))])
+        })
+    );
 
-    println!("{:?}", res);
+    let res = parse_curly(r#"{%  youtube id="movie"  title="Title" answer=42 %}"#);
+    println!("{:?}\n", res);
+    assert_eq!(
+        res,
+        Some(Curly {
+            name: String::from("youtube"),
+            fields: HashMap::from([
+                (String::from("id"), String::from("movie")),
+                (String::from("title"), String::from("Title"))
+            ])
+        })
+    );
 }
 
-
 fn parse_curly(text: &str) -> Option<Curly> {
-    let re = Regex::new(r#"\{%\s+(?<name>[a-z]+)\s+(([a-z]+)="([^"]+)"\s+)*%\}"#).unwrap();
-    match re.captures(text) {
+    let base_pattern = r#"
+        \{%
+        \s+
+        ([a-z]+)
+        \s+
+        (.*)
+        %\}
+        "#;
+    // ((([a-z]+)="([^"]+)"\s+)*)
+    let base_re = RegexBuilder::new(base_pattern)
+        .ignore_whitespace(true)
+        .build()
+        .unwrap();
+
+    let pair_pattern = r#"
+    ([a-z]+)="([^"]+)"
+    \s+
+    (.*)
+    "#;
+
+    let pair_re = RegexBuilder::new(pair_pattern)
+        .ignore_whitespace(true)
+        .build()
+        .unwrap();
+
+    let mut locs = base_re.capture_locations();
+    match base_re.captures_read(&mut locs, text) {
         None => None,
-        Some(cap) => {
-            //cap.
-            Some(Curly {
-                name: cap["name"].to_owned(),
-                fields: HashMap::new(),
-            })
-            
+        Some(_cap) => {
+            let loc = locs.get(1).unwrap();
+            let name = &text[loc.0..loc.1];
+            println!("name: {name}");
+
+            let loc = locs.get(2).unwrap();
+            let mut text = &text[loc.0..loc.1];
+            println!("text {text}");
+
+            let mut crl = Curly {
+                name: name.to_string(),
+                ..Curly::default()
+            };
+
+            loop {
+                if text.is_empty() {
+                    break;
+                }
+
+                let mut locs = pair_re.capture_locations();
+                println!("TEXT: {text}");
+                match pair_re.captures_read(&mut locs, text) {
+                    None => break,
+                    Some(_cap) => {
+                        let loc = locs.get(1).unwrap();
+                        let field = &text[loc.0..loc.1];
+                        println!("field: '{field}'");
+
+                        let loc = locs.get(2).unwrap();
+                        let value = &text[loc.0..loc.1];
+                        println!("value: '{value}'");
+                        crl.fields.insert(field.to_owned(), value.to_owned());
+
+                        match locs.get(3) {
+                            None => {
+                                println!("break");
+                                break;
+                            }
+                            Some(loc) => {
+                                text = &text[loc.0..loc.1];
+                                println!("new text: {text}");
+                            }
+                        }
+                    }
+                }
+            }
+
+            Some(crl)
         }
     }
 }
