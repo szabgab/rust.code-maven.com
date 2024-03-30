b67668f70a2880d636cfbc1d0063fcf909e87d0a
Gabor Szabo <gabor@szabgab.com>
2024-03-30 08:33:11 +0300

    publish

--- examples/memory-allocation/src/main.rs
+++ examples/memory-allocation/src/main.rs
@@ -3,43 +3,41 @@ use sysinfo::System;
 use thousands::Separable;
 
 fn main() {
-    println!("Total memory: {}",  total_memory().separate_with_commas());
     let filler = "0123456789".repeat(10);
     let size = 200_000_000;
 
     println!(
-        "Size: {} filler: {} total: {}",
+        "Size: {} filler: {} total: {}\n",
         size.separate_with_commas(),
         filler.len(),
         (size * filler.len()).separate_with_commas()
     );
 
-    let used_before = show_memory();
-    println!("Used memory before: {}",  used_before.separate_with_commas());
+    println!("Total memory:                        {:>15}",  total_memory().separate_with_commas());
+    let used_before = used_memory();
+    println!("Used memory before allocation:       {:>15}", used_before.separate_with_commas());
 
-    allocate(size, &filler);
+    let used_allocated = allocate(size, &filler);
+    println!("Used memory after allocation:        {:>15}", used_allocated.separate_with_commas());
 
-    println!(
-        "Memory used since start of the program: {}",
-        (used_allocated - used_before).separate_with_commas()
+    let used_after = used_memory();
+    println!("Used memory after deallocation:      {:>15}", used_after.separate_with_commas());
+
+    println!("Memory used by allocation (diff):    {:>15}", (used_allocated - used_before).separate_with_commas()
+    );
+    println!("Memory freed by deallocation (diff): {:>15}", (used_allocated - used_after).separate_with_commas()
     );
 
-    let used_after = show_memory();
-    println!("Used memory after: {}",  used_after.separate_with_commas());
-
-    //allocate(size, &filler);
 }
 
-fn allocate(size: usize, filler: &str) {
+fn allocate(size: usize, filler: &str) -> u64 {
 
     let mut text = String::with_capacity(size * filler.len());
     for _ in 0..size {
         text.push_str(filler);
     }
 
-    let used_allocated = show_memory();
-    println!("Used memory allocated: {}",  used_allocated.separate_with_commas());
-
+    used_memory()
 }
 
 
---
+++ pages/memory-allocation-and-usage.md
@@ -0,0 +1,22 @@
+---
+title: Memory allocation and usage in Rust
+timestamp: 2024-03-30T08:30:01
+author: szabgab
+published: true
+description:
+tags:
+    - Rust
+todo:
+    - memory usage of current process?
+---
+
+
+## Dependencies in Cargo.toml
+
+{% include file="examples/memory-allocation/Cargo.toml" %}
+
+## The full code
+
+{% include file="examples/memory-allocation/src/main.rs" %}
+
+
