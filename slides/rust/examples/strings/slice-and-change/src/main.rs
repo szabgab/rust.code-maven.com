067bfdf2aa9cf29d25be8aae5bb6715e4a2c4ecc
Gabor Szabo <gabor@szabgab.com>
2024-07-06 18:01:35 +0300

    String::from

--- slides/rust/examples/strings/slice-and-change/src/main.rs
+++ slides/rust/examples/strings/slice-and-change/src/main.rs
@@ -1,8 +1,10 @@
 fn main() {
-    let mut text = "Foobar".to_string();
+    let mut text = String::from("Foobar");
     println!("{}", text);
+
     let slice = &text[0..=2];
     println!("{}", slice);
+
     text.clear();
     text.push_str("qwerty");
     println!("{}", text);
