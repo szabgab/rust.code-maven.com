d864dc255e12498fe8f1d5781675dc23d7074ed5
Gabor Szabo <gabor@szabgab.com>
2024-07-13 10:16:19 +0300

    update

---
+++ slides/rust/examples/threads/simple/src/main.rs
@@ -0,0 +1,17 @@
+use std::thread;
+
+fn main() {
+    println!("Before starting: {:?}", thread::current().id());
+
+    thread::spawn(|| {
+        println!("First thread {:?}", thread::current().id());
+    });
+
+    // let handle = thread::spawn(|| {
+    //        println!("Second thread {:?}", thread::current().id());
+    // });
+
+    // handle.join().unwrap(); 
+
+    println!("After ending: {:?}", thread::current().id());
+}
--- slides/rust/examples/threads/threads-load-test/src/main.rs
+++ slides/rust/examples/threads/threads-load-test/src/main.rs
@@ -10,35 +10,19 @@ fn main() {
     // 42: 18.1 sec vs 3.3 sec
     let args = env::args().collect::<Vec<String>>();
     if args.len() != 3 {
-        eprintln!("Usage: {} count []", args[0]);
+        eprintln!("Usage: {} n [linear|threads]", args[0]);
         process::exit(1);
     }
     let n = args[1].parse::<u64>().expect(format!("Could not convert {} to integer", args[1]).as_str());
 
+    let repetition = 10;
 
     let start = Instant::now();
     if args[2] == "linear" {
-        println!("single thread");
-        for _x in 1..=10 {
-            println!("{n}: {}", fibonacci(n));
-        }
+        linear(n, repetition);
     } else if args[2] == "threads" {
-        println!("multiple threads");
-        let (tx, rx) = mpsc::channel();
+        in_threads(n, repetition);
 
-        for _x in 1..=10 {
-            let txr = tx.clone();
-            thread::spawn(move || {
-                let res = fibonacci(n);
-                txr.send(res.to_string()).unwrap();
-                println!("spawned thread finished");
-            });
-        }
-        drop(tx);
-    
-        for received in rx {
-            println!("Got: {}", received);
-        }    
     } else {
         println!("Invalid parameter {}", args[2])
     }
@@ -54,4 +38,30 @@ fn fibonacci(n :u64) -> u64 {
         return 1;
     }
     fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+fn linear(n: u64, repetition: i32) {
+    println!("single thread");
+    for _x in 1..=repetition {
+        println!("{n}: {}", fibonacci(n));
+    }
+}
+
+fn in_threads(n: u64, repetition: i32) {
+    println!("multiple threads");
+    let (tx, rx) = mpsc::channel();
+
+    for _x in 1..=repetition {
+        let txr = tx.clone();
+        thread::spawn(move || {
+            let res = fibonacci(n);
+            txr.send(res.to_string()).unwrap();
+            println!("spawned thread finished");
+        });
+    }
+    drop(tx); // need to drop in the main thread
+
+    for received in rx {
+        println!("Got: {}", received);
+    }    
 }
