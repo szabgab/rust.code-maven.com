55d00ef5f1f8ada8f01327379aeb9bf75d11150f
Gabor Szabo <gabor@szabgab.com>
2024-07-16 15:31:29 +0300

    Extend string

--- slides/rust/examples/vectors/extend-strings/src/main.rs
+++ slides/rust/examples/vectors/extend-strings/src/main.rs
@@ -12,20 +12,24 @@ macro_rules! prt {
 }
 
 
-
 fn main() {
     let mut fruits1 = vec![String::from("apple"), String::from("banana")];
     prt!(fruits1);
 
-    let fruits2 = vec![String::from("peach"), String::from("kiwi")];
+    let mut fruits2 = vec![String::from("peach"), String::from("kiwi"), String::from("mango")];
     prt!(fruits2);
 
-    fruits1.extend(&fruits2);
+    fruits1.extend(fruits2.clone());
     prt!(fruits1);
-//    prt!(fruits1[2]);
-    // prt!(fruits2);
+    prt!(fruits2);
 
-    // fruits1[2] = "egg";
-    // prt!(fruits1);
-    // prt!(fruits2);
+    prt!(fruits1[3]);
+    prt!(fruits2[1]);
+
+    fruits2[1] = String::from("some fruit with a very long name that requires more memory than we have");
+    prt!(fruits1[3]);
+    prt!(fruits2[1]);
+
+    prt!(fruits1);
+    prt!(fruits2);
 }
