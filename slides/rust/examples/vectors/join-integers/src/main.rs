82c5794abdf1c89637148596c8a3bcb3ec76b0a8
Gabor Szabo <gabor@szabgab.com>
2024-07-17 19:29:52 +0300

    format

--- slides/rust/examples/vectors/join-integers/src/main.rs
+++ slides/rust/examples/vectors/join-integers/src/main.rs
@@ -2,7 +2,11 @@ fn main() {
     let integers = vec![1, 2, 3];
     println!("{:?}", integers);
 
-    let joined = integers.iter().map(|num| format!("{num}")).collect::<Vec<_>>().join(",");
+    let joined = integers
+        .iter()
+        .map(|num| num.to_string())
+        .collect::<Vec<_>>()
+        .join(",");
     println!("{}", joined);
     assert_eq!(joined, "1,2,3");
 }
