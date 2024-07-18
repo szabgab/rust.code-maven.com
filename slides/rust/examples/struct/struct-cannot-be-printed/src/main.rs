90e2728ccc6ae8cf651e0dc71bd5112756cae49b
Gabor Szabo <gabor@szabgab.com>
2024-07-18 10:01:53 +0300

    add

--- slides/rust/examples/struct/struct-cannot-be-printed/src/main.rs
+++ slides/rust/examples/struct/struct-cannot-be-printed/src/main.rs
@@ -5,6 +5,8 @@ struct Red(i32);
 fn main() {
     #[allow(unused_variables)]
     let red = Red(10);
+    println!("{}", red.0);
+
     // println!("{}", red);
     // `Red` doesn't implement `std::fmt::Display`
 
