25c044b53c919e0b045e7a470ba029742a31d62a
Gabor Szabo <gabor@szabgab.com>
2024-07-18 08:16:15 +0300

    exercise

---
+++ slides/rust/examples/struct/phonebook/src/main.rs
@@ -0,0 +1,64 @@
+#[allow(dead_code)]
+#[derive(Debug)]
+enum Title {
+    Doctor,
+    Prof,
+}
+
+
+
+#[allow(dead_code)]
+#[derive(Debug)]
+enum PhoneType {
+    Home,
+    Work,
+    Mobile,
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Phone {
+    number: String,
+    ptype: PhoneType,
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Person {
+    name: String,
+    id: String,
+    phones: Vec<Phone>,
+    notes: Option<String>,
+    title: Option<Title>,
+}
+
+fn main() {
+    let joe = Person {
+        name: String::from("Joe"),
+        id: String::from("123456"),
+        phones: vec![
+            Phone {
+                number: String::from("055-1234567"),
+                ptype: PhoneType::Home,
+            }
+        ],
+        notes: None,
+        title: None,
+    };
+
+    let jane = Person {
+        name: String::from("Jane"),
+        id: String::from("678"),
+        phones: vec![
+            Phone {
+                number: String::from("123"),
+                ptype: PhoneType::Work,
+            }
+        ],
+        notes: Some(String::from("was a student")),
+        title: Some(Title::Doctor)
+    };
+
+    println!("{:#?}", joe);
+    println!("{:#?}", jane);
+}
--- slides/rust/struct.md
+++ slides/rust/struct.md
@@ -285,4 +285,23 @@
 
 ![](examples/struct/implement-ordering/out.out)
 
+## Exercise - struct for contact info
+{id: exercise-struct-for-contact-info}
+
+Create the code in steps. In each step create the struct add some data, and print it.
+
+* 1. At first define a person that has a `name`, an `id`.
+
+* 2. Each person can have several phone numbers. Each phone number has a type and a number. The type might be "Home" or "Work" or "Mobile".
+
+* 3. We might want to add some notes to some of the people.
+
+* 4. Some people might have a title (e.g. "Doctor", "Prof")
+
+## Solution - struct for contact info
+{id: solution-struct-for-contact-info}
+
+![](examples/struct/phonebook/src/main.rs)
+
+![](examples/struct/phonebook/out.out)
 
