fd133acda1c9478c4856d370b3d5a5deb5240658
Gabor Szabo <gabor@szabgab.com>
2024-07-18 21:04:57 +0300

    fmt

--- slides/rust/examples/lifetime/function-one-param/src/main.rs
+++ slides/rust/examples/lifetime/function-one-param/src/main.rs
@@ -2,16 +2,16 @@ fn main() {
     let a = String::from("Foo");
     let choice = true;
 
-    let c = select(&a, choice);    
+    let c = select(&a, choice);
 
     println!("{}", c);
-    println!("{}", a);    
-
+    println!("{}", a);
 }
 
-
-fn select<>(name1: &str, choice: bool) -> &str {
-    if choice { name1 } else { name1 }
+fn select(name1: &str, choice: bool) -> &str {
+    if choice {
+        name1
+    } else {
+        name1
+    }
 }
-
-
