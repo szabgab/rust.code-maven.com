4448da3ac69f4ec2d17336a45804d36b329b4602
Gabor Szabo <gabor@szabgab.com>
2024-07-19 05:48:16 +0300

    lifetime

--- slides/rust/examples/lifetime/function-one-param/src/main.rs
+++ slides/rust/examples/lifetime/function-one-param/src/main.rs
@@ -1,17 +1,23 @@
 fn main() {
-    let a = String::from("Foo");
-    let choice = true;
+    let mut a = String::new();
+    println!("{a}");
+    a = String::from("before");
+    println!("{a}");
 
-    let c = select(&a, choice);
+    let c = select(&a);
+    a = String::from("after");
 
     println!("{}", c);
     println!("{}", a);
 }
 
-fn select(name1: &str, choice: bool) -> &str {
-    if choice {
-        name1
+//fn select(text: &str) -> &str {
+//fn select<'a>(text: &'a str) -> &'a str {
+//fn select<'a, 'b>(text: &'a str) -> &'b str {
+fn select(text: &str) -> &'static str {
+    if text > "abc" {
+        "first"
     } else {
-        name1
+        "second"
     }
 }
--- slides/rust/examples/lifetime/function-two-params/src/main.rs
+++ slides/rust/examples/lifetime/function-two-params/src/main.rs
@@ -1,22 +1,37 @@
 fn main() {
-    let a = String::from("Foo");
-    let b = String::from("Bar");
-    let choice = true;
+    let mut a = String::new();
+    let mut b = String::new();
+    println!("a {a}");
+    println!("b {b}");
 
-    let c = select(&a, &b, choice);
+    a = String::from("AAA");
+    b = String::from("BBB");
 
-    println!("{}", c);
-    println!("{}", a);
+    let c = select(&a, &b);
+    a = String::from("aaa");
+    b = String::from("bbb");
+
+    println!("c {c}");
+    println!("a {a}");
+    println!("b {b}");
 }
 
-// fn select<>(name1: &str, name2: &str, choice: bool) -> &str {
+// fn select(name1: &str, name2: &str, choice: bool) -> &str {
 //     if choice { name1 } else { name2 }
 // }
 
-fn select<'a>(name1: &'a str, name2: &'a str, choice: bool) -> &'a str {
-    if choice {
-        name1
+//fn select<'a>(name1: &'a str, name2: &'a str) -> &'a str {
+//    if name1 > name2 {
+//        name1
+//    } else {
+//        name2
+//    }
+//}
+
+fn select<'a>(name1: &str, name2: &str) -> &'a str {
+    if name1 > name2 {
+        "first"
     } else {
-        name2
+        "second"
     }
 }
