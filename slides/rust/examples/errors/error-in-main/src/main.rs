85206203b60af146fc70ac50d30e668e3d7ae446
Gabor Szabo <gabor@szabgab.com>
2024-07-21 16:58:58 +0300

    layout

--- slides/rust/examples/errors/error-in-main/src/main.rs
+++ slides/rust/examples/errors/error-in-main/src/main.rs
@@ -3,9 +3,16 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = std::env::args().collect::<Vec<_>>();
 
-    let filename = &args.get(1).ok_or("Missing file")?;
-    let number = args[2].parse::<u8>()?;
-    let num = number.checked_add(255).ok_or("overflow")?;
+    let filename = &args
+        .get(1)
+        .ok_or("Missing file")?;
+    let number = args
+        .get(2)
+        .ok_or("Param 2 was missing")?
+        .parse::<u8>()?;
+    let num = number
+        .checked_add(255)
+        .ok_or("overflow")?;
 
 
     let content = std::fs::read_to_string(filename)?;
