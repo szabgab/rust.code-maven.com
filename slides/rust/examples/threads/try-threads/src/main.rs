66f95d861aad120b88b0c9b90fbfd0c5ffea85b1
Gabor Szabo <gabor@szabgab.com>
2024-07-22 13:25:04 +0300

    update

--- slides/rust/examples/threads/try-threads/src/main.rs
+++ slides/rust/examples/threads/try-threads/src/main.rs
@@ -5,14 +5,16 @@ fn main() {
     println!("Before starting: {:?}", thread::current().id());
 
     let handle = thread::spawn(|| {
-        for i in 1..10 {
+        for i in 1..=10 {
             println!("Hi number {} from the spawned thread! {:?}", i, thread::current().id());
             thread::sleep(Duration::from_millis(1));
         }
         println!("Spawned thread ended");
     });
 
-    for i in 1..5 {
+    //thread::sleep(Duration::from_millis(1));
+    //thread::sleep(Duration::from_micros(1));
+    for i in 1..=5 {
         println!("Hi number {} from the main    thread! {:?}", i, thread::current().id());
         thread::sleep(Duration::from_millis(1));
     }
