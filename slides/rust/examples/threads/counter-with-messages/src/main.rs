fc15e1423414ab8e5ecb084647dd967872781a17
Gabor Szabo <gabor@szabgab.com>
2024-07-22 12:55:31 +0300

    update

--- slides/rust/examples/threads/counter-with-messages/src/main.rs
+++ slides/rust/examples/threads/counter-with-messages/src/main.rs
@@ -1,24 +1,27 @@
 fn main() {
-    let limit = 100;
+    let threads = 10;
+    let limit = 1000;
 
-    let result = count_with_messages(limit);
+    let result = count_with_messages(threads, limit);
     println!("{}", result);
-    assert_eq!(result, limit);
+    assert_eq!(result, threads*limit);
 }
 
-fn count_with_messages(limit: i32) -> i32 {
+fn count_with_messages(threads: i32, limit: i32) -> i32 {
     let mut counter = 0;
-    let (tx1, rx) = std::sync::mpsc::channel();
-    for _ in 0..limit {
-        let tx2 = tx1.clone();
+    let (tx, rx) = std::sync::mpsc::channel();
+    for _ in 1..=threads {
+        let local_tx = tx.clone();
 
         std::thread::spawn(move || {
             //println!("{:?}", std::thread::current().id());
-            tx2.send(1).unwrap();
+            for _ in 1..=limit {
+                local_tx.send(1).unwrap();
+            }
         });
     }
 
-    drop(tx1); // we need to close this Sender so the next loop will end properly
+    drop(tx); // we need to close this Sender so the next loop will end properly
 
     for received in rx {
         counter += received;
