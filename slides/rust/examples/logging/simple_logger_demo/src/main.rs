459964f8c848dea4a2338bf50ec6db4238868024
Gabor Szabo <gabor@szabgab.com>
2024-08-04 15:09:09 +0300

    update

--- slides/rust/examples/logging/simple_logger_demo/src/main.rs
+++ slides/rust/examples/logging/simple_logger_demo/src/main.rs
@@ -1,9 +1,13 @@
-use simple_logger::SimpleLogger;
+//use simple_logger::SimpleLogger;
 
 fn main() {
-    SimpleLogger::new().init().unwrap();
+    simple_logger::init().unwrap();
+    //SimpleLogger::new().init().unwrap();
 
+    log::trace!("This is a sample trace.");
+    log::debug!("This is a sample debug.");
     log::info!("This is a sample info.");
     log::warn!("This is a sample warn.");
+    log::error!("This is a sample error.");
 }
 
