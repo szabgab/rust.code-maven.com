f2447503f2275419f2c41e46bf4a982894bb0071
Gabor Szabo <gabor@szabgab.com>
2024-08-06 17:29:30 +0300

    add operator

--- slides/rust/examples/rocket/calculator-with-get/src/main.rs
+++ slides/rust/examples/rocket/calculator-with-get/src/main.rs
@@ -3,10 +3,33 @@ extern crate rocket;
 
 use rocket::response::content;
 
-#[get("/?<a>&<b>")]
-fn index(a: Option<i64>, b: Option<i64>) -> content::RawHtml<String> {
-    let result = match (a, b) {
-        (Some(a), Some(b)) => (a + b).to_string(),
+#[get("/?<a>&<b>&<op>")]
+fn index(a: Option<i64>, b: Option<i64>, op: Option<String>) -> content::RawHtml<String> {
+    let mut selected_add = "";
+    let mut selected_multiply = "";
+    let mut selected_subtract = "";
+    let mut selected_divide = "";
+
+    let result = match (a, b, op) {
+        (Some(a), Some(b), Some(op)) => match op.as_ref() {
+            "add" => {
+                selected_add = r#"selected="selected""#;
+                (a + b).to_string()
+            }
+            "subtract" => {
+                selected_subtract = r#"selected="selected""#;
+                (a - b).to_string()
+            }
+            "divide" => {
+                selected_divide = r#"selected="selected""#;
+                (a / b).to_string()
+            }
+            "multiply" => {
+                selected_multiply = r#"selected="selected""#;
+                (a * b).to_string()
+            }
+            _ => String::new(),
+        },
         _ => String::new(),
     };
 
@@ -23,7 +46,15 @@ fn index(a: Option<i64>, b: Option<i64>) -> content::RawHtml<String> {
     let mut html = format!(
         r#"
     <form>
-    <input name="a" value="{a}"> <input name="b" value="{b}">
+    <input name="a" value="{a}">
+    <input name="b" value="{b}">
+    <select name="op">
+    <option value="add" {selected_add}>+</option>
+    <option value="multiply" {selected_multiply}>*</option>
+    <option value="subtract" {selected_subtract}>-</option>
+    <option value="divide" {selected_divide}>/</option>
+    </select>
+
     <input type="submit" value="Calculate">
     "#
     );
