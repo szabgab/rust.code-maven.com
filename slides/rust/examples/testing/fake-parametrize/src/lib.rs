8af8586ac9874fe8cbaa41c30b73ec82f3ea7a2a
Gabor Szabo <gabor@szabgab.com>
2024-08-18 09:13:31 +0300

    add

---
+++ slides/rust/examples/testing/fake-parametrize/src/lib.rs
@@ -0,0 +1,26 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+
+        let result = add(3, 3);
+        assert_eq!(result, 6);
+    }
+
+    #[test] fn it_works_1_2() { test_add(1, 2, 3) }
+    #[test] fn it_works_2_3() { test_add(2, 3, 5) }
+
+    fn test_add(a: u64, b: u64, expected: u64) {
+        let result = add(a, b);
+        assert_eq!(result, expected);
+    }
+
+}
--- slides/rust/testing.md
+++ slides/rust/testing.md
@@ -96,3 +96,11 @@ TODO
 ![](examples/testing/test-crate/Cargo.toml)
 ![](examples/testing/test-crate/src/main.rs)
 
+## Parametrization of tests
+{id: parametrizations-of-tests}
+
+I could not find parametrization as exists in pytest, but we can fake it by creating a function that accepts
+the parameters and then creating many test functions calling that function.
+
+![](examples/testing/fake-parametrize/src/lib.rs)
+
