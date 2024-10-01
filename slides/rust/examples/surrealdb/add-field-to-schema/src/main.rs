69b899a4a21a74c622f0330d82a8f4c2f76ebbf8
Gabor Szabo <gabor@szabgab.com>
2024-10-01 13:30:00 +0300

    add

---
+++ slides/rust/examples/surrealdb/add-field-to-schema/src/main.rs
@@ -0,0 +1,76 @@
+use serde::{Deserialize, Serialize};
+use surrealdb::engine::local::Mem;
+use surrealdb::sql::Thing;
+use surrealdb::Surreal;
+
+
+#[derive(Debug, Deserialize, Serialize)]
+struct EntryNummer {
+    id: Thing,
+    number: u32,
+}
+
+
+#[derive(Debug, Deserialize, Serialize)]
+struct EntryWithName {
+    id: Thing,
+    number: u32,
+    name: Option<String>,
+}
+
+#[tokio::main]
+async fn main() -> surrealdb::Result<()> {
+    let dbh = Surreal::new::<Mem>(()).await?;
+
+    dbh.use_ns("demo").use_db("demo").await?;
+
+    dbh.query("DEFINE TABLE entry SCHEMAFULL").await?;
+    dbh.query("DEFINE FIELD number ON TABLE entry TYPE int").await?;
+
+
+    let res = dbh.query("CREATE entry CONTENT {
+        number: 42,
+
+    };").await?;
+    match res.check() {
+        Ok(val) => println!("Success: {val:?}"),
+        Err(err) => println!("Error: {err}"),
+    }
+    println!("---------");
+
+
+    let mut entries = dbh.query("SELECT * FROM entry").await?;
+    let entries: Vec<EntryNummer> = entries.take(0)?;
+    for entry in entries {
+        println!("{} {}", entry.id, entry.number);
+    }
+    println!("---------");
+
+    let res = dbh.query("DEFINE FIELD name ON TABLE entry TYPE string").await?;
+    match res.check() {
+        Ok(val) => println!("Success: {val:?}"),
+        Err(err) => println!("Error: {err}"),
+    }
+    println!("---------");
+
+    let res = dbh.query("CREATE entry CONTENT {
+        number: 23,
+        name: 'twenty three',
+    };").await?;
+    match res.check() {
+        Ok(val) => println!("Success: {val:?}"),
+        Err(err) => println!("Error: {err}"),
+    }
+    println!("---------");
+
+
+    let mut entries = dbh.query("SELECT * FROM entry").await?;
+    let entries: Vec<EntryWithName> = entries.take(0)?;
+    for entry in entries {
+        println!("{} {} {}", entry.id, entry.number, entry.name.unwrap_or("missing".to_string()));
+    }
+    println!("---------");
+
+    Ok(())
+}
+
--- slides/rust/surrealdb.md
+++ slides/rust/surrealdb.md
@@ -308,12 +308,21 @@ Other message
 {id: surrealdb-missing-field}
 
 * We have a SCHEMAFULL table but not all the fields are supplied.
+* The CREATE will fail on missing fields
 
 ![](examples/surrealdb/missing-field/src/main.rs)
 ![](examples/surrealdb/missing-field/out.out)
 
+## SurrealDB - add field to schema
+{id: surrealdb-add-field-to-schema}
 
+* We have a schema and some entries in the databas.
+* We change the schema adding another field, but that field does not exist in the data that is already in the database.
+* For new data we have to supply the new field as well.
+* For old data we don't have to make changes but when we SELECT the data we need to have a struct where this field is `Option`.
 
+![](examples/surrealdb/add-field-to-schema/src/main.rs)
+![](examples/surrealdb/add-field-to-schema/out.out)
 
 ## SurrealDB in Docker using the CLI
 {id: surrealdb-in-docker-using-cli}
