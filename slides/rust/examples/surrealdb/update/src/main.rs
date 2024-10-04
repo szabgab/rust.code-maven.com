666c29f8f955b42057fffae838370b6c07913e0e
Gabor Szabo <gabor@szabgab.com>
2024-10-04 13:48:36 +0300

    upgrade and move to memory

--- slides/rust/examples/surrealdb/update/src/main.rs
+++ slides/rust/examples/surrealdb/update/src/main.rs
@@ -1,7 +1,5 @@
 use serde::{Deserialize, Serialize};
-use surrealdb::engine::remote::ws::Client;
-use surrealdb::engine::remote::ws::Ws;
-//use surrealdb::opt::Resource;
+use surrealdb::engine::local::{Db, Mem};
 use surrealdb::Surreal;
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -12,11 +10,11 @@ struct Fruit {
 
 #[tokio::main]
 async fn main() -> surrealdb::Result<()> {
-    let db = Surreal::new::<Ws>("127.0.0.1:8000").await?;
-    db.use_ns("demo").use_db("demo-time").await?;
+    let dbh = Surreal::new::<Mem>(()).await?;
+    dbh.use_ns("demo").use_db("demo-time").await?;
 
-    let _response = db.query("DELETE fruits").await?.check();
-    list(&db).await?;
+    let _response = dbh.query("DELETE fruits").await?.check();
+    list(&dbh).await?;
     let mut id = 0;
 
     let fruits = vec![
@@ -32,35 +30,35 @@ async fn main() -> surrealdb::Result<()> {
 
     for raw_fruit in fruits {
         id += 1;
-        let fruit: Fruit = db.create(("fruits", id)).content(raw_fruit).await?.unwrap();
-        println!("db: {:?}", fruit);
+        let fruit: Fruit = dbh.create(("fruits", id)).content(raw_fruit).await?.unwrap();
+        println!("fruit: {:?}", fruit);
     }
-    list(&db).await?;
+    list(&dbh).await?;
 
-    let _response = db
+    let _response = dbh
         .query("UPDATE fruits SET color=$color WHERE name=$name")
         .bind(("name", "apple"))
         .bind(("color", "red"))
         .await?
         .check();
     //println!("{:?}", response);
-    list(&db).await?;
+    list(&dbh).await?;
 
     let fruit = Fruit {
         name: String::from("apple"),
         color: String::from("green"),
     };
 
-    //let _result = db.update(Resource::from("fruits")).content(fruit).await?;
+    //let _result = dbh.update(Resource::from("fruits")).content(fruit).await?;
 
-    let _fruit: Option<Fruit> = db.update(("fruits", 1)).content(fruit).await?;
+    let _fruit: Option<Fruit> = dbh.update(("fruits", 1)).content(fruit).await?;
 
-    list(&db).await?;
+    list(&dbh).await?;
     Ok(())
 }
 
-async fn list(db: &Surreal<Client>) -> surrealdb::Result<()> {
-    let fruits: Vec<Fruit> = db.select("fruits").await?;
+async fn list(dbh: &Surreal<Db>) -> surrealdb::Result<()> {
+    let fruits: Vec<Fruit> = dbh.select("fruits").await?;
     println!("List:");
     for fruit in fruits {
         println!("   {:?}", fruit);
