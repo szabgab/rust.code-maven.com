35547401937c158194cf76bb78895de379f9e94b
Gabor Szabo <gabor@szabgab.com>
2024-10-16 11:52:24 +0300

    update

--- slides/rust/examples/vectors/sort-strings/src/main.rs
+++ slides/rust/examples/vectors/sort-strings/src/main.rs
@@ -1,15 +1,29 @@
 fn main() {
-    let mut animals = vec![
-        String::from("snake"),
-        String::from("crab"),
-        String::from("camel"),
-        String::from("elephant"),
-        String::from("lizard"),
-    ];
+    let mut animals = get_animals();
+
     println!("{animals:?}");
+    assert_eq!(animals, ["snake", "crab", "elephant", "lizard"]);
+
     animals.sort();
     println!("{animals:?}");
+    assert_eq!(animals, ["crab", "elephant", "lizard", "snake"]);
 
+    #[allow(clippy::unnecessary_sort_by)]
     animals.sort_by(|a, b| a.len().cmp(&b.len()));
     println!("{animals:?}");
+    assert_eq!(animals, ["crab", "snake", "lizard", "elephant"]);
+
+    let mut animals = get_animals();
+    animals.sort_by_key(|a| a.len());
+    println!("{animals:?}");
+    assert_eq!(animals, ["crab", "snake", "lizard", "elephant"]);
+}
+
+fn get_animals() -> Vec<String> {
+    vec![
+        String::from("snake"),
+        String::from("crab"),
+        String::from("elephant"),
+        String::from("lizard"),
+    ]
 }
