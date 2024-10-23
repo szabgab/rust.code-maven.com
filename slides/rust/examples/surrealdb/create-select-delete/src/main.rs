171743288e9430201c9da1984b8f2320a0741a44
Gabor Szabo <gabor@szabgab.com>
2024-10-23 20:04:25 +0300

    update and use memory

--- slides/rust/examples/surrealdb/create-select-delete/src/main.rs
+++ slides/rust/examples/surrealdb/create-select-delete/src/main.rs
@@ -1,6 +1,5 @@
 use serde::{Deserialize, Serialize};
-use surrealdb::engine::remote::ws::Client;
-use surrealdb::engine::remote::ws::Ws;
+use surrealdb::engine::local::{Db, Mem};
 use surrealdb::opt::Resource;
 use surrealdb::Surreal;
 
@@ -11,38 +10,47 @@ struct Fruit {
 
 #[tokio::main]
 async fn main() -> surrealdb::Result<()> {
-    let db = Surreal::new::<Ws>("127.0.0.1:8000").await?;
-    db.use_ns("demo").use_db("demo-time").await?;
+    let dbh = Surreal::new::<Mem>(()).await?;
 
-    let _response = db.query("DELETE fruits").await?.check();
-    list(&db).await?;
+    dbh.use_ns("demo").use_db("demo-time").await?;
+
+    let list = get_list(&dbh).await?;
+    show_list(&list);
+    assert!(list.is_empty());
 
     for name in ["apple", "banana"] {
         let fruit = Fruit {
             name: name.to_owned(),
         };
-        let _result = db.create(Resource::from("fruits")).content(fruit).await?;
+        let _result = dbh.create(Resource::from("fruits")).content(fruit).await?;
         //println!("{}", result);
     }
-    list(&db).await?;
+    let list = get_list(&dbh).await?;
+    show_list(&list);
+    assert_eq!(list.len(), 2);
 
-    let _response = db
+    let _response = dbh
         .query("DELETE fruits WHERE name=$name")
         .bind(("name", "apple"))
         .await?
         .check();
     //println!("{:?}", response);
-    list(&db).await?;
+    let list = get_list(&dbh).await?;
+    show_list(&list);
+    assert_eq!(list.len(), 1);
+    assert_eq!(list[0].name, "banana");
 
     Ok(())
 }
 
-async fn list(db: &Surreal<Client>) -> surrealdb::Result<()> {
-    let fruits: Vec<Fruit> = db.select("fruits").await?;
+async fn get_list(dbh: &Surreal<Db>) -> surrealdb::Result<Vec<Fruit>> {
+    let fruits: Vec<Fruit> = dbh.select("fruits").await?;
+    Ok(fruits)
+}
+
+fn show_list(list: &Vec<Fruit>) {
     println!("List:");
-    for fruit in fruits {
+    for fruit in list {
         println!("   {:?}", fruit);
     }
-
-    Ok(())
 }
