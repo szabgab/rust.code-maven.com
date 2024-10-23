0e6a61244fbf4554934943a321bf89d3b3bcbe0c
Gabor Szabo <gabor@szabgab.com>
2024-10-23 12:28:53 +0300

    assert

--- slides/rust/examples/hashes/total-values/src/main.rs
+++ slides/rust/examples/hashes/total-values/src/main.rs
@@ -9,9 +9,13 @@ fn main() {
         *total.entry(key).or_insert(0) += value;
     }
     println!("{:#?}", total);
+    assert_eq!(total, a);
 
     for (key, value) in b.iter() {
         *total.entry(key).or_insert(0) += value;
     }
     println!("{:#?}", total);
+
+    let expected_total = HashMap::from([("apple", 3), ("peach", 2), ("grape", 2), ("banana", 1)]);
+    assert_eq!(total, expected_total);
 }
