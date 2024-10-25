9079b01a3725152b514a10c183ac7085d9eee65b
Gabor Szabo <gabor@szabgab.com>
2024-10-25 20:07:32 +0300

    progress report

--- slides/rust/examples/surrealdb/connect-to-server/src/main.rs
+++ slides/rust/examples/surrealdb/connect-to-server/src/main.rs
@@ -4,17 +4,17 @@ use surrealdb::{Error, Surreal};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    println!("Connect to server:");
+    println!("Connect to server.");
     let db = Surreal::new::<Ws>("localhost:8000").await?;
 
-    println!("Authenticating as the root user:");
+    println!("Authenticating as the root user.");
     db.signin(Root {
         username: "root",
         password: "root",
     })
     .await?;
 
-    println!("Select a specific namespace and database:");
+    println!("Select a specific namespace and database.");
     db.use_ns("namespace").use_db("database").await?;
 
     Ok(())
