74a14cb1c8d642c89a4e701eb9b983631a0d3148
Gabor Szabo <gabor@szabgab.com>
2024-10-27 08:16:30 +0200

    update surrealdb; fix code; add asserts

--- slides/rust/examples/surrealdb/insert-select-in-memory/src/main.rs
+++ slides/rust/examples/surrealdb/insert-select-in-memory/src/main.rs
@@ -9,6 +9,12 @@ struct Message {
     text: String,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct MessageWithId {
+    id: Thing,
+    text: String,
+}
+
 #[derive(Debug, Deserialize)]
 struct Record {
     #[allow(dead_code)]
@@ -17,33 +23,51 @@ struct Record {
 
 #[tokio::main]
 async fn main() -> surrealdb::Result<()> {
-    let db = Surreal::new::<Mem>(()).await?;
+    let dbh = Surreal::new::<Mem>(()).await?;
 
-    db.use_ns("demo").use_db("insert-select-in-memory").await?;
+    dbh.use_ns("demo").use_db("insert-select-in-memory").await?;
 
-    create_a_new_message_with_random_id(&db, "Hello World").await?;
-    create_a_new_message_with_random_id(&db, "Another message").await?;
+    create_a_new_message_with_random_id(&dbh, "Hello World").await?;
+    create_a_new_message_with_random_id(&dbh, "Another message").await?;
 
-    select_all_the_messages(&db).await?;
-
-    Ok(())
-}
-
-async fn select_all_the_messages(db: &Surreal<Db>) -> surrealdb::Result<()> {
-    let mut response = db.query("SELECT * from messages").await?;
-    let messages: Vec<Message> = response.take(0)?;
+    let messages = select_all_the_messages(&dbh).await?;
     for message in &messages {
-        println!("{}", message.text);
+        println!("{:?}", message);
+    }
+    assert_eq!(messages.len(), 2);
+    assert_eq!(messages[0].text, "Another message");
+    assert_eq!(messages[1].text, "Hello World");
+    println!();
+
+    let messages = select_all_the_messages_with_id(&dbh).await?;
+    for message in &messages {
+        println!("{:?}", message);
     }
 
     Ok(())
 }
 
+async fn select_all_the_messages(dbh: &Surreal<Db>) -> surrealdb::Result<Vec<Message>> {
+    let mut response = dbh.query("SELECT * from messages ORDER BY text").await?;
+    let messages: Vec<Message> = response.take(0)?;
+
+    Ok(messages)
+}
+
+async fn select_all_the_messages_with_id(
+    dbh: &Surreal<Db>,
+) -> surrealdb::Result<Vec<MessageWithId>> {
+    let mut response = dbh.query("SELECT * from messages ORDER BY name").await?;
+    let messages: Vec<MessageWithId> = response.take(0)?;
+
+    Ok(messages)
+}
+
 async fn create_a_new_message_with_random_id(
-    db: &Surreal<Db>,
+    dbh: &Surreal<Db>,
     message: &str,
 ) -> surrealdb::Result<()> {
-    let created: Vec<Record> = db
+    let created: Option<Record> = dbh
         .create("messages")
         .content(Message {
             text: message.to_owned(),
