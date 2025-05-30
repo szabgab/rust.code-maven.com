3fb0c6a792db11c4f6406721059a6fd7fd75ca04
Gabor Szabo <gabor@szabgab.com>
2025-05-30 12:03:38 +0300

    path

--- books.sh
+++ books.sh
@@ -7,7 +7,7 @@ do
     echo Book $book
 
     cd books/$book
-    ./mdbook build
+    ../../mdbook build
     cd ../..
     perl -i -p -e 's{href="index.html"}{href="."}g'           books/${book}/book/toc.js
     perl -i -p -e 's{(href="[a-zA-Z0-9.][^"]+)\.html"}{$1"}g' books/${book}/book/toc.js
