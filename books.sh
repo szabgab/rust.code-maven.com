ec753bddf881333b9b4a77fe62c767999be64e5c
Gabor Szabo <gabor@szabgab.com>
2025-05-30 11:36:22 +0300

    cd to make relative include links work

--- books.sh
+++ books.sh
@@ -6,7 +6,9 @@ for book in csv logging why-rust regex json yaml sqlite
 do
     echo Book $book
 
-    ./mdbook build books/$book
+    cd books/$book
+    ./mdbook build
+    cd ../..
     perl -i -p -e 's{href="index.html"}{href="."}g'           books/${book}/book/toc.js
     perl -i -p -e 's{(href="[a-zA-Z0-9.][^"]+)\.html"}{$1"}g' books/${book}/book/toc.js
     find books/${book}/book/ -name *.html | xargs perl -i -p -e 's{href="index.html"}{href="."}g'
