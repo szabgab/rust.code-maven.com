94883a878bd7b3776ea70224463f4882172a910d
Gabor Szabo <gabor@szabgab.com>
2025-07-03 12:41:14 +0300

    use mdbook in the path which is set in the CI

--- books.sh
+++ books.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash -ex
 
-mkdir _site/books
+mkdir -p _site/books
 
 for book in clap csv json logging regex reqwest sqlite testing why-rust yaml
 do
     echo Book $book
 
     cd books/$book
-    ../../mdbook build
+    mdbook build
     cd ../..
     perl -i -p -e 's{href="index.html"}{href="."}g'           books/${book}/book/toc.js
     perl -i -p -e 's{(href="[a-zA-Z0-9.][^"]+)\.html"}{$1"}g' books/${book}/book/toc.js
