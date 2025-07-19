c8b60252ed89d2f37decb354d847dae31388ddf5
Gabor Szabo <gabor@szabgab.com>
2025-07-19 08:23:52 +0200

    get the names from the folders

--- books.sh
+++ books.sh
@@ -2,19 +2,22 @@
 
 mkdir -p _site/books
 
-for book in clap csv json logging regex reqwest rust-programming sqlite testing why-rust yaml
+for full_book in books/*
 do
-    echo Book $book
+    if [ -d "$full_book" ]; then
+        book=$(basename "$full_book")
+        echo Book $book
 
-    cd books/$book
-    mdbook build
-    cd ../..
-    perl -i -p -e 's{href="index.html"}{href="."}g'           books/${book}/book/toc.js
-    perl -i -p -e 's{(href="[a-zA-Z0-9.][^"]+)\.html"}{$1"}g' books/${book}/book/toc.js
-    find books/${book}/book/ -name *.html | xargs perl -i -p -e 's{href="index.html"}{href="."}g'
-    find books/${book}/book/ -name *.html | xargs perl -i -p -e 's{(href="[a-zA-Z0-9.][^"]+)\.html"}{$1"}g'
+        cd books/$book
+        mdbook build
+        cd ../..
+        perl -i -p -e 's{href="index.html"}{href="."}g'           books/${book}/book/toc.js
+        perl -i -p -e 's{(href="[a-zA-Z0-9.][^"]+)\.html"}{$1"}g' books/${book}/book/toc.js
+        find books/${book}/book/ -name *.html | xargs perl -i -p -e 's{href="index.html"}{href="."}g'
+        find books/${book}/book/ -name *.html | xargs perl -i -p -e 's{(href="[a-zA-Z0-9.][^"]+)\.html"}{$1"}g'
 
-    mv books/${book}/book _site/$book
+        mv books/${book}/book _site/$book
+    fi
 
 done
 
