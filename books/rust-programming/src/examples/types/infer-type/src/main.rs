6c1689a4333375529ea24ba3642a4f16d9be9821
Gabor Szabo <gabor@szabgab.com>
2025-08-19 08:32:03 +0300

    make the example thinner

--- books/rust-programming/src/examples/types/infer-type/src/main.rs
+++ books/rust-programming/src/examples/types/infer-type/src/main.rs
@@ -7,10 +7,12 @@ fn main() {
 
     let z = 42; // at first Rust will assume this is i32, the default
 
-    let result = y + z; // When Rust sees this it will understand that z participates
-                        // in a operation where both operands have to be the same type and the other operand
-                        // was explicitely set to be i8. So Rust infers that z is also of type i8.
-                        // It also infers that "result" will be of type i8.
+    let result = y + z; 
+    // When Rust sees this it will understand that "z" participates
+    // in a operation where both operands have to be the same type.
+    // The variable "y", the other operand, was explicitely set to be i8.
+    // So Rust infers that z must be also of type i8.
+    // It also infers that the variable "result" will be of type i8.
 
     println!("{result}");
 }
--- books/rust-programming/src/rust/types/infer-the-type-of-variable.md
+++ books/rust-programming/src/rust/types/infer-the-type-of-variable.md
@@ -1,7 +1,7 @@
 # Rust infers the type of variables
 
-* Infer / deduct the type from the other types of other variables and from the operation on the variable.
-* "guess the type" is probably not a good methaphore as there is no guessing in it.
+* Rust Infers (deducts) the type from the other types of other variables and from the operation on the variable.
+* "guess the type" is probably not a good metaphor as there is no guessing in it.
 
 {% embed include file="src/examples/types/infer-type/src/main.rs" %}
 
