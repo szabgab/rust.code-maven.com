90b2ff7c5b29f333959bdb751688d8d751dccc6d
Gabor Szabo <gabor@szabgab.com>
2025-09-17 11:56:50 +0300

    more interesting

--- books/rust-async/src/examples/tokio/join-set-order/src/main.rs
+++ books/rust-async/src/examples/tokio/join-set-order/src/main.rs
@@ -1,7 +1,14 @@
 #[tokio::main]
 async fn main() {
     let mut tasks = tokio::task::JoinSet::new();
-    for i in 0..10 {
+
+    tasks.spawn(async move {
+        println!("Task long starts");
+        tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
+        println!("Task long is done");
+    });
+
+    for i in 0..5 {
         let time = rand::random::<u64>() % 100;
         println!("Task {i} setup for {time}");
         tasks.spawn(async move {
@@ -10,5 +17,10 @@ async fn main() {
             println!("Task {i} is done");
         });
     }
+
+    println!("All tasks started");
+    std::thread::sleep(std::time::Duration::from_secs(1));
+    println!("Wait done");
+
     tasks.join_all().await;
 }
