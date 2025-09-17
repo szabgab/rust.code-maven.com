33a7ac3817d0b71b8a6c9d0af78c0933b0121225
Gabor Szabo <gabor@szabgab.com>
2025-09-17 15:07:12 +0300

    logging

--- books/rust-async/src/examples/tokio/join-set-order/src/main.rs
+++ books/rust-async/src/examples/tokio/join-set-order/src/main.rs
@@ -1,30 +1,31 @@
 #[tokio::main]
 async fn main() {
-    println!("Start");
+    simple_logger::SimpleLogger::new().init().unwrap();
+    log::info!("Start");
     let mut tasks = tokio::task::JoinSet::new();
 
     tasks.spawn(async move {
-        println!("Task long starts");
+        log::info!("Task long starts");
         tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
-        println!("Task long is done");
+        log::info!("Task long is done");
     });
     std::thread::sleep(std::time::Duration::from_secs(1));
-    println!("Long task started");
+    log::info!("Long task started");
 
     for i in 0..5 {
         let time = rand::random::<u64>() % 100;
-        println!("Task {i} setup for {time}");
+        log::info!("Task {i} setup for {time}");
         tasks.spawn(async move {
-            println!("Task {i} starts");
+            log::info!("Task {i} starts");
             tokio::time::sleep(tokio::time::Duration::from_millis(time)).await;
-            println!("Task {i} is done");
+            log::info!("Task {i} is done");
         });
     }
 
-    println!("All tasks started");
+    log::info!("All tasks started");
     std::thread::sleep(std::time::Duration::from_secs(1));
-    println!("Wait done");
+    log::info!("Wait done");
 
     tasks.join_all().await;
-    println!("End");
+    log::info!("End");
 }
