d5bb91e8732f7a9c47bd5c747022793cd8649e79
Gabor Szabo <gabor@szabgab.com>
2025-09-19 08:52:36 +0300

    improve example

--- books/rust-async/src/examples/tokio/sync-vs-async/src/main.rs
+++ books/rust-async/src/examples/tokio/sync-vs-async/src/main.rs
@@ -1,18 +1,3 @@
-#[tokio::main]
-async fn main() {
-    println!("Start");
-    let value = sync_func();
-    println!("Value: {}", value);
-
-    let future = async_func();
-
-    println!("Before await");
-    let res = future.await;
-    println!("Result: {}", res);
-
-    println!("End");
-}
-
 fn sync_func() -> u32 {
     println!("sync_func called");
     42
@@ -22,3 +7,21 @@ async fn async_func() -> u32 {
     println!("async_func called");
     42
 }
+
+#[tokio::main]
+async fn main() {
+    println!("Start");
+    let value = sync_func();
+    println!("Value: {}", value);
+
+    println!("Before calling async_func");
+    let future = async_func();
+
+    println!("Before await");
+    let res = future.await;
+    println!("Result: {}", res);
+
+    println!("End");
+}
+
+
