d9b36d929bb6488bf28b048ea4ca40d0edd49b06
Gabor Szabo <gabor@szabgab.com>
2025-12-25 13:23:40 +0100

    publish

---
+++ examples/mail-sender/src/main.rs
@@ -0,0 +1,41 @@
+use clap::Parser;
+use lettre::{FileTransport, Message, SendmailTransport, Transport, message::header::ContentType};
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(long)]
+    from: String,
+
+    #[arg(long)]
+    to: String,
+
+    #[arg(long)]
+    subject: String,
+
+    #[arg(long)]
+    dir: Option<String>,
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    let email = Message::builder()
+        .from(args.from.parse().unwrap())
+        .to(args.to.parse().unwrap())
+        .subject(args.subject)
+        .header(ContentType::TEXT_PLAIN)
+        .body(String::from("Body"))
+        .unwrap();
+
+    match args.dir {
+        Some(dirname) => {
+            let sender = FileTransport::new(dirname);
+            let filename = sender.send(&email).unwrap();
+            println!("{filename}.eml");
+        }
+        None => {
+            let sender = SendmailTransport::new();
+            sender.send(&email).unwrap();
+        }
+    }
+}
---
+++ pages/mail-sender.md
@@ -0,0 +1,17 @@
+---
+title: Mail sender using the lettre crate
+timestamp: 2025-12-25T13:30:01
+author: szabgab
+published: true
+show_related: true
+description:
+tags:
+    - lettre
+---
+
+[lettre](https://crates.io/crates/lettre)
+
+{% include file="examples/mail-sender/Cargo.toml" %}
+
+{% include file="examples/mail-sender/src/main.rs" %}
+
