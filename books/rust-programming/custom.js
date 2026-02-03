aeddfd06e4ab560497688b16e86bd87f4efd3b6f
Gabor Szabo <gabor@szabgab.com>
2026-02-03 11:48:49 +0200

    separate the configuration and the code

--- books/rust-programming/custom.js
+++ books/rust-programming/custom.js
@@ -1,34 +1,4 @@
+const prefixes = ["src/examples/"];
 const extensions = [".rs", ".py", ".toml"];
 const base_url = "https://github.com/szabgab/rust.code-maven.com/tree/main/books/rust-programming/"
 
-function linkIncludeFiles() {
-  document.querySelectorAll("div[data-embedify][data-app='include']").forEach((node) => {
-    const file = node.getAttribute("data-option-file");
-    console.log(`Try ${file}`);
-    if (!file) return;
-
-    // Only handle files inside src/examples/
-    if (!file.startsWith("src/examples/")) return;
-
-    extensions.forEach((ext) => {
-        if (file.endsWith(ext)) {
-            createLink(node, file);
-            return;
-        }
-    });
-  });
-}
-
-function createLink(node, file) {
-    // Create a link to the corresponding GitHub file
-    const a = document.createElement("a");
-    a.href = `${base_url}${file}`;
-    a.textContent = file;
-    a.target = "_blank";
-
-    console.log("Insert the link after the current node");
-    node.insertAdjacentElement("afterend", a);
-}
-
-console.log("add event listener");
-document.addEventListener("DOMContentLoaded", linkIncludeFiles);
