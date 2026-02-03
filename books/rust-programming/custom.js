5a23ee35ee4e845c895a98637517451a8863f6ea
Gabor Szabo <gabor@szabgab.com>
2026-02-03 11:10:57 +0200

    move some configuration variables to external const. Add logging

--- books/rust-programming/custom.js
+++ books/rust-programming/custom.js
@@ -1,23 +1,33 @@
+const extensions = [".rs", ".py", ".toml"];
+const base_url = "https://github.com/szabgab/rust.code-maven.com/tree/main/books/rust-programming/"
+
 function linkIncludeFiles() {
   document.querySelectorAll("div[data-embedify][data-app='include']").forEach((node) => {
     const file = node.getAttribute("data-option-file");
+    console.log(`Try ${file}`);
     if (!file) return;
 
     // Only handle files inside src/examples/
     if (!file.startsWith("src/examples/")) return;
 
-    // Only handle .rs and .py files
-    if (!file.endsWith(".rs") && !file.endsWith(".py")) return;
+    extensions.forEach((ext) => {
+        if (file.endsWith(ext)) {
+            createLink(node, file);
+            return;
+        }
+    });
+  });
+}
 
+function createLink(node, file) {
     // Create a link to the corresponding GitHub file
     const a = document.createElement("a");
-    a.href = `https://github.com/szabgab/rust.code-maven.com/tree/main/books/rust-programming/${file}`;
+    a.href = `${base_url}${file}`;
     a.textContent = file;
     a.target = "_blank";
 
-    // Insert the link after the current node
+    console.log("Insert the link after the current node");
     node.insertAdjacentElement("afterend", a);
-  });
 }
 
 document.addEventListener("DOMContentLoaded", linkIncludeFiles);
